886335693a57c77b34ddbe96fb7f2c7d80f0e6b9
Sebastian Strobl <sebastian.strobl@gmx.de>
2022-09-01 16:25:35 +0200

    Update SHT31TempHumiditySensor.ino
    Description updated

--- examples/SHT31TempHumiditySensor/SHT31TempHumiditySensor.ino
+++ examples/SHT31TempHumiditySensor/SHT31TempHumiditySensor.ino
@@ -2,8 +2,8 @@
   SHT31TempHumiditySensor.ino
 
   This is an example for the SHT31 Temperature & Humidity Sensor.
-  The sensor uses I2C to communicate, 2 pins are required to
-  interface.
+  The sensor uses I2C to communicate, 2 pins are required to interface. 
+  This example requires Adafruit SHT31 Libray to be installed.
 
   Copyright (c) by Sebastian Strobl,
   More information on: https://github.com/sebaJoSt/BlaeckSerial
